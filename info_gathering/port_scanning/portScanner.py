8fdabd6d44e4754ad2598cd7d5d635b1128d2d19
Daniel Edwards <dannygithub@gmail.com>
2017-03-14 19:38:06 -0400

    using socket.socket connection

--- info_gathering/port_scanning/portScanner.py
+++ info_gathering/port_scanning/portScanner.py
@@ -1,55 +1,46 @@
 import argparse, sys, datetime, time, re
 from socket import *
 
-#Create socket object
-global maxPort, minPort
-maxPort=7000
-minPort=1
-
-
 def _con_scan(host, port):
-    global sock
-    print("BUT I CAN'T SEE YOU!!")
+
     try:
         #global sock
-        sock = socket(AF_INET, socket.SOCK_STREAM)
+        sock = socket(AF_INET, SOCK_STREAM)
         sock.connect(host, port)
-        #sock.send('GitHub\r\n')
-        results = sock.recv(1024); #Receive no more than 1024 bytes
+        sock.send('ViolentPython\r\n')
+        results = sock.recv(100); #Receive no more than 1024 bytes
         print("$ TCP Port: " + port + " open")
-        print("$" + str(results))
+        print(results)
     except:
-        print("- TCP Port: ", port, "closed")
-    #finally:
-        #sock.close()
+        print("$ TCP Port: "  + port + " closed\n")
+    finally:
+        sock.close()
 
 #After connecting to target, scan target.
 def _scan_port(host, ports):
-    #print("IDK IF THAT'S YOU!!!!")
-    #_con_scan(host, port)
     try:
         host_ip = gethostbyname(host) #takes host name, returns host ip
-        print("Scan initiated @ " + time.strftime("%H:%M:%S") + "\n")
+        print("$ Scan initiated @ " + time.strftime("%H:%M:%S") + "\n_______________________________")
+        #d = date.fromordinal(730920)  # 730920th day after 1. 1. 0001
 
-        startTime=datetime.now()
     except:
-        print("ERROR. Unknown host ", host_ip)
+        print("ERROR. Unknown host " + host_ip)
 
     try:
         hostname=gethostbyaddr(host_ip)
-        print("\nHostname Results: " + hostname)
+        print("\n$ Hostname Results: " + hostname)
     except:
-        print("\nIP Results: " + host_ip)
+        print("\n$ IP Results: " + host_ip)
 
     for port in ports:
-        print("Scanning Port: " + port)
+        print("\n$ Scanning Port: " + port)
+        _con_scan(host_ip,port)
 
 
 def main():
-    print("CAN YOU SEE ME???")
-    parser = argparse.ArgumentParser('usage -h <target host> -p <target port>')
-    parser.add_argument('-H', dest='tgtPort', required=True, type=str, help='specify target host')
-    parser.add_argument('-p', dest='tgtHost', required=True, type=str, help='specify target port[s] separated by commas')
+    parser = argparse.ArgumentParser('usage -help <target host> -p <target port(s)>')
+    parser.add_argument('-H', dest='tgtHost', required=True, type=str, help='specify target host')
+    parser.add_argument('-p', dest='tgtPort', required=True, type=str)
 
     #Gets the target host and target port
     args = parser.parse_args()
@@ -57,9 +48,9 @@ def main():
     tgtPorts = str(args.tgtPort).split(',')
     _scan_port(tgtHost, tgtPorts)
 
-
-    if (tgtHost == None) | (tgtPorts[0] == None):
+    if tgtHost is None or tgtPorts[0] is None:
         print(parser.usage)
     exit(0)
+
 if __name__ == '__main__':
     main()
