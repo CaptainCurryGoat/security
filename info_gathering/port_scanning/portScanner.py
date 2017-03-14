69b4740ffb4bbc5970a5ef7107a67334b40afcd0
Daniel Edwards <dannygithub@gmail.com>
2017-03-14 15:26:19 -0400

    adding for loop in _scan_port

--- info_gathering/port_scanning/portScanner.py
+++ info_gathering/port_scanning/portScanner.py
@@ -8,34 +8,41 @@ minPort=1
 
 
 def _con_scan(host, port):
+    global sock
     print("BUT I CAN'T SEE YOU!!")
     try:
         #global sock
         sock = socket(AF_INET, socket.SOCK_STREAM)
-        sock = socket.connect(host, port)
+        sock.connect(host, port)
+        #sock.send('GitHub\r\n')
         results = sock.recv(1024); #Receive no more than 1024 bytes
-
-        print("TCP Port: ", port, " open")
-        print(str(results))
-
+        print("$ TCP Port: " + port + " open")
+        print("$" + str(results))
     except:
-        print("TCP Port: ", port, "closed")
-
-    finally:
-        sock.close()
+        print("- TCP Port: ", port, "closed")
+    #finally:
+        #sock.close()
 
 #After connecting to target, scan target.
-def _scan_port(host, port):
-    print("IDK IF THAT'S YOU!!!!")
-    _con_scan(host, port)
+def _scan_port(host, ports):
+    #print("IDK IF THAT'S YOU!!!!")
+    #_con_scan(host, port)
     try:
         host_ip = gethostbyname(host) #takes host name, returns host ip
-        print("Host: ", host_ip, "\n")
-        print("Scan initiated @ ", time.strftime("%H:%M:%S"), "\n")
+        print("Scan initiated @ " + time.strftime("%H:%M:%S") + "\n")
 
         startTime=datetime.now()
     except:
-        print("ERROR. Unknown host ", host)
+        print("ERROR. Unknown host ", host_ip)
+
+    try:
+        hostname=gethostbyaddr(host_ip)
+        print("\nHostname Results: " + hostname)
+    except:
+        print("\nIP Results: " + host_ip)
+
+    for port in ports:
+        print("Scanning Port: " + port)
 
 
 def main():
